1110c45d512b5638b84d1c8c6a424db0e1d7cbea
CannedFish <lianggy0719@126.com>
2015-03-24 18:38:03 +0800

    sth

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MusicEdu' });
+  res.render('index', {
+    title: 'MusicEdu',
+    submit: 'ChaXun',
+    register: 'ZhuCe',
+    signIn: 'Denglu',
+    home: 'Zhuye'
+  });
 });
 
 module.exports = router;
